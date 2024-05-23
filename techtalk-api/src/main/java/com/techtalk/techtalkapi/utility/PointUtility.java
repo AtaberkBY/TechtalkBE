f46afa515a90026dc8999ed6ce86216ae7dfed25
Berke <berkecxl@gmail.com>
2024-05-23 21:19:02 +0300

    TEC-63 added title update to PointUtility

--- techtalk-api/src/main/java/com/techtalk/techtalkapi/utility/PointUtility.java
+++ techtalk-api/src/main/java/com/techtalk/techtalkapi/utility/PointUtility.java
@@ -23,16 +23,25 @@ public class PointUtility {
     public void updateLevel(User user) {
         double currentPoints = user.getPoint();
         int level = user.getLevel();
+        String title = user.getTitle();
+
+        if(title == null) {
+        user.setTitle("Unemployed");
+        }
 
         if (currentPoints >= 240) {
+            title = "Senior";
             level = 3;
         } else if (currentPoints >= 120) {
             level = 2;
+            title = "Junior";
         } else if (currentPoints >= 50) {
             level = 1;
+            title = "Intern";
         }
 
         if (user.getLevel() != level) {
+            user.setTitle(title);
             user.setLevel(level);
         }
     }
